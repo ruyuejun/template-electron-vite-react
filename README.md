da6bfab3b842fe30614f6ecb8b72ae81ce1c317d
ruyuejun <99316632@qq.com>
2023-06-21 21:31:09 +0800

    feat: init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+自用的 Electron-react 模版。默认配置了 husky、commitizen
---
+++ electron/main.ts
@@ -0,0 +1,111 @@
+import { app, BrowserWindow, shell, ipcMain } from 'electron'
+import { release } from 'node:os'
+import { join } from 'node:path'
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+process.env.DIST_ELECTRON = join(__dirname, '..')
+process.env.DIST = join(__dirname, '../dist')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(process.env.DIST, '../public')
+
+// Disable GPU Acceleration for Windows 7
+if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+
+// Set application name for Windows 10+ notifications
+if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+
+if (!app.requestSingleInstanceLock()) {
+    app.quit()
+    process.exit(0)
+}
+
+let win: BrowserWindow | null
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+// Here, you can also use other preload
+const preload = join(__dirname, 'preload.js')
+const url = process.env.VITE_DEV_SERVER_URL
+const indexHtml = join(process.env.DIST, 'index.html')
+
+function createWindow() {
+    win = new BrowserWindow({
+        icon: join(process.env.PUBLIC, 'electron-vite.svg'),
+        webPreferences: {
+            preload: preload
+        }
+    })
+
+    // Test active push message to Renderer-process.
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
+
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL)
+        win.webContents.openDevTools()
+    } else {
+        // win.loadFile('dist/index.html')
+        win.loadFile(join(process.env.DIST, 'index.html'))
+    }
+
+    // Test actively push message to the Electron-Renderer
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
+
+    // Make all links open with the browser, not with the application
+    win.webContents.setWindowOpenHandler(({ url }) => {
+        if (url.startsWith('https:')) shell.openExternal(url)
+        return { action: 'deny' }
+    })
+    // win.webContents.on('will-navigate', (event, url) => { }) #344
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    win = null
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+    if (win) {
+        // Focus on the main window if the user tried to open another
+        if (win.isMinimized()) win.restore()
+        win.focus()
+    }
+})
+
+app.on('activate', () => {
+    const allWindows = BrowserWindow.getAllWindows()
+    if (allWindows.length) {
+        allWindows[0].focus()
+    } else {
+        createWindow()
+    }
+})
+
+// New window example arg: new windows url
+ipcMain.handle('open-win', (_, arg) => {
+    const childWindow = new BrowserWindow({
+        webPreferences: {
+            preload,
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    if (process.env.VITE_DEV_SERVER_URL) {
+        childWindow.loadURL(`${url}#${arg}`)
+    } else {
+        childWindow.loadFile(indexHtml, { hash: arg })
+    }
+})
